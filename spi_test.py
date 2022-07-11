b5a4d948cdd2a7dab44a286827a7c13ef26fabec
Kirill Trunov <trunov.kb@phystech.edu>
2022-07-11 17:15:40 +0300

    working on spi

--- spi_test.py
+++ spi_test.py
@@ -2,18 +2,19 @@ import spidev
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 1600000
+spi.max_speed_hz = 1000000
 
 def getAdc():
-    adcResponse = spi.xfer2([0, 0])
-    return ((adcResponse[0] & 0x1F) << 8 | adcResponse[1]) >> 1
+    adcResponse = spi.xfer2([0])
+#    return ((adcResponse[0] & 0x80) >> 7) | ((adcResponse[0] & 0x7F) << 1)
+    return adcResponse[0]
 
 try:
     samples = []
-    
-    for i in range(20000):
+
+    for i in range(13):
         samples.append(getAdc())
-    print(samples[:10])
+    print(samples)
         
 finally:
     spi.close()
